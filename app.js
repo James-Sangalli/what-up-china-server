43f0d5ac82bb72edbf906d9489c5c36558f06ce9
James Sangalli <j.l.sangalli@gmail.com>
2021-05-10 00:15:17 +1000

    improve handling

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ app.get("/top-news/", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
     const query = `https://newsapi.org/v2/top-headlines?country=cn&apiKey=6d7709b0ec234faab6e438466941c2ae`;
     request.get(query).then((data) => {
-        res.send(JSON.parse(data.text.articles));
-    }).catch(res.send);
+        console.log("in then callback");
+        res.send(JSON.parse(data.text).articles);
+    }).catch((e) => {
+        console.error(e);
+        res.send(e);
+    });
 });
 
 app.listen(port, () => console.log(`listening at ${port}`));
