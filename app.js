e059ed31dea672f4a35f5ebf33701a6f0d07c040
James Sangalli <j.l.sangalli@gmail.com>
2021-05-10 12:20:41 +1000

    use multiple API keys

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const request = require('superagent');
+const apiKeysNews = [
+    "6d7709b0ec234faab6e438466941c2ae",
+    "15e281928b994633ab09b55784ce35cd",
+    "0e9d878eeb994e5087ad30e40e5706db"
+]
 
 app.get("/top-news/", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
-    const query = `https://newsapi.org/v2/top-headlines?country=cn&apiKey=6d7709b0ec234faab6e438466941c2ae`;
-    request.get(query).then((data) => {
-        console.log("in then callback");
-        res.send(JSON.parse(data.text).articles);
-    }).catch((e) => {
-        console.error(e);
-        res.send(e);
+    callNewsAPI(0).then((response) => {
+        res.send(response);
+    }).catch((err) => {
+        res.send(err);
     });
 });
+
+async function callNewsAPI(apiKeyIndex) {
+    const query = `https://newsapi.org/v2/top-headlines?country=cn&apiKey=${apiKeysNews[apiKeyIndex]}`;
+    try {
+        let result = await request.get(query);
+        return JSON.parse(result.text).articles;
+    } catch (e) {
+        console.error(e);
+        if(e.message === "Too Many Requests") {
+            return callNewsAPI(++apiKeyIndex);
+        } else {
+            return e;
+        }
+    }
+}
 
 app.listen(port, () => console.log(`listening at ${port}`));
