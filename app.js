cde4a697e41227cef889899b2b6241fc187858eb
James Sangalli <j.l.sangalli@gmail.com>
2021-05-16 16:15:56 +1000

    add baidu news

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const request = require('superagent');
+const parse = require("node-html-parser").parse;
 const apiKeysNews = [
     "6d7709b0ec234faab6e438466941c2ae",
     "15e281928b994633ab09b55784ce35cd",
     "0e9d878eeb994e5087ad30e40e5706db"
 ]
+
+app.get("/baidu/hot-news/", (req, res, next) => {
+    res.header( 'Access-Control-Allow-Origin','*' );
+    getBaiduHotNews().then((response) => {
+        res.send(response);
+    }).catch((err) => {
+        res.send(err);
+    });
+});
 
 app.get("/top-news/", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
@@ -39,6 +49,34 @@ async function callNewsAPI(apiKeyIndex) {
         } else {
             return e;
         }
+    }
+}
+
+//TODO this is gross
+async function getBaiduHotNews() {
+    const query = "https://news.baidu.com/";
+    const articles = [];
+    try {
+        let result = await request.get(query);
+        let parsedHtml = parse(result.text);
+        const topNews = parsedHtml.querySelector(".hotnews").childNodes[1];
+        for(let i = 0; i < topNews.childNodes.length; i+=2) {
+            const section = topNews.childNodes[i].childNodes.toString();
+            const link = section.match(/\bhttps?:\/\/\S+/gi)[0].replace('"', "");
+            let title = section.match(/\p{Script=Han}+/gu);
+            title = title[title.length - 1];
+            if(!link.includes("<strong>")) {
+                articles.push({
+                    title: title,
+                    link: link
+                });
+            }
+        }
+
+        return articles;
+    } catch (e) {
+        console.error(e);
+        return e;
     }
 }
 
